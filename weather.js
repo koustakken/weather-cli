aa50a26fa8f22381c97194d79e1792025b094695
Kous <kous.takken@gmail.com>
2023-06-10 23:20:00 +0300

    get args

--- weather.js
+++ weather.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
 
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+    const args = getArgs(process.argv)
+    
+    if ( args.h ) {
+        // Вывод help
+    }
+
+    if ( args.s ) {
+        // Сохранить города
+    }
+
+    if ( args.t ) {
+        // Сохранить токен
+    }
+
+    // Вывести погоду
+}
+
+initCLI()
